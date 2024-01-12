5f58cc7c975d3ca31265d89d10ab2f110c8cabe7
shaonbishwas <shaonbishwas01@gmail.com>
2024-01-13 01:31:15 +0800

    error page edit

--- client-side/src/Pages/Error/Error.jsx
+++ client-side/src/Pages/Error/Error.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Error = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen">
             <img src="https://i.postimg.cc/rs6cs7pK/Error-Page.gif" alt="Error Icon" />
+            <Link to='/'><button className=" border border-[#11B0F3] py-1 px-5 ">Back To Home</button></Link>
         </div>
     );
 };
